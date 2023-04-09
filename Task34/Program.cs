c91b3c65546a0841f933170c57d1f1d1f770c4fe
Eva <okamenoame@gmail.com>
2023-04-09 21:34:23 +0300

    task 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива:  ");
+int size = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+//PrintArray(numbers);
+int count = 0;
+
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    count++;
+}
+
+Console.WriteLine($"В массиве всего {numbers.Length} чисел, {count} из них - чётные.");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
